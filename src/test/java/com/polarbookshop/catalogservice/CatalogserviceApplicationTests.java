b86897bf99008a562191395def7379b85ea97973
hongs429 <honshyeon@gmail.com>
2025-06-13 00:47:30 +0900

    fix : 테스트컨테이너로 실제환경과 동일한 환경에서 테스트 진행

--- src/test/java/com/polarbookshop/catalogservice/CatalogserviceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogserviceApplicationTests.java
@@ -7,10 +7,13 @@ import com.polarbookshop.catalogservice.persistence.BookRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class CatalogserviceApplicationTests {
--- src/test/java/com/polarbookshop/catalogservice/service/CatalogServiceIntegrationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/service/CatalogServiceIntegrationTests.java
@@ -8,11 +8,14 @@ import com.polarbookshop.catalogservice.persistence.BookRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(
         webEnvironment = WebEnvironment.RANDOM_PORT // 완전한 스프링 웹 애플리케이션 콘텍스트와 임의의 포트를 듣는 서블릿 컨테이너를 로드
 )
